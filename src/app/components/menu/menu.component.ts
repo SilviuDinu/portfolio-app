6e0160e1992e3d72a7f358bef270d529fcc8a477
SilviuDinu <silviu_dinu@yahoo.com>
2021-07-30 20:42:03 +0300

    added routes

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { routes } from '@constants/routes';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-  constructor() {}
+export class MenuComponent implements OnInit {
+  routes: any[] = routes;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  navigateTo(url: string) {
+    this.router.navigateByUrl(url);
+  }
 }
---
+++ src/app/constants/routes.ts
@@ -0,0 +1,22 @@
+export const routes = [
+  {
+    name: 'Home',
+    link: '/'
+  },
+  {
+    name: 'About me',
+    link: '/about-me'
+  },
+  {
+    name: 'Resume',
+    link: '/resume'
+  },
+  {
+    name: 'Projects',
+    link: '/projects'
+  },
+  {
+    name: 'Contact',
+    link: '/contact'
+  },
+]
