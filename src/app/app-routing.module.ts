22bdbf59582627335288219d249d30c507cbd982
SilviuDinu <silviu_dinu@yahoo.com>
2021-08-01 21:18:33 +0300

    start defining routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { routes as paths } from '@constants/routes';
 
-const routes: Routes = paths
-  .filter((path: any) => !path.isExternal)
-  .map((route: any) => {
-    return {
-      path: route.path,
-      component: route.component,
-    };
-  });
+// const routes: Routes = paths
+//   .filter((path: any) => !path.isExternal)
+//   .map((route: any) => {
+//     return {
+//       path: route.path,
+//       component: route.component,
+//     };
+//   });
 
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
